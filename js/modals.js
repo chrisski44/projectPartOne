b73c90a3069fb9013eb059e7f3c8e4f55cede0ec
chrisski44 <chris.skowronski@outlook.com>
2016-10-13 11:49:11 -0400

    Adding styles and js

---
+++ js/modals.js
@@ -0,0 +1,22 @@
+function modalOne(){
+    var modalOne = document.getElementById('modalOne');
+    var btn = document.getElementById('btnOne');
+     
+    modalOne.style.display = 'block';
+}
+
+function modalTwo(){
+    var modalTwo = document.getElementById('modalTwo');
+    var btn = document.getElementById('btnTwo');
+     
+    modalTwo.style.display = 'block';
+}
+
+function modalThree(){
+    var modalThree = document.getElementById('modalThree');
+    var btn = document.getElementById('btnThree');
+     
+    modalThree.style.display = 'block';
+}
+
+
