71a1a1a314d30462b2e20874a70c26122e3bcbf4
Chris Skowronski <chris.skowronski@outlook.com>
2016-10-13 19:19:35 -0400

    Giving credit where its due

--- js/menuScroll.js
+++ js/menuScroll.js
@@ -7,3 +7,5 @@
             }, 750);
         }
     });
+//Shoutout to this dude not making me include a plugin to do the simple menu scroll
+//https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
